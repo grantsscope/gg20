d38c9f55fd4060131ea64df24b9f8610741a827b
rohitmalekar <rohitmalekar@gmail.com>
2024-04-29 23:23:46 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -55,7 +55,7 @@ st.sidebar.markdown("- About [GrantsScope](http://grantsscope.xyz/)")
 
 st.title('GrantsScope - GG20')
 st.markdown('Ask away your questions to learn more about the grantees in GG20. Here are a few sample questions to start your discovery:')
-st.markdown('*- Tell me about projects critical for Ethereum\'s  
+st.markdown('*- Tell me about projects critical for Ethereum\'s  \
 infrastructure.*')
 st.markdown('*- Give me examples of real-world impact for grantees in climate round.*')
 st.markdown('*- ELI5 few projects in token engineering round to me.*')
